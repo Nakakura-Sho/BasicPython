1f5bb5fefd4a5e9427b5548eb7a3a38e818dc9fc
Nakakura-Sho <108964516+Nakakura-Sho@users.noreply.github.com>
2024-02-15 09:02:18 +0900

    制御構文の回答

--- machine_epsilon.py
+++ machine_epsilon.py
@@ -1,1 +1,7 @@
 # TODO
+epsilon=1
+while epsilon + 1 != 1:
+    
+    epsilon=epsilon/2
+
+print(epsilon*2)
--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,21 @@
 a = input("aの値を入力: ")
-b = input("bの値を入力: ")
+#b = input("bの値を入力: ")
 
 # TODO
+n = a
+is_prime = True
+
+# 2から n-1 までの数で割り切れるかどうか
+for p in range(2, n):
+    if n % p == 0:
+        is_prime = False
+        break
+
+# 1と自分自身以外で割り切れなければ素数
+if n == 1:
+    is_prime = False
+
+if is_prime:
+    print(str(n) + ' は素数である。')
+else:
+    print(str(n) + ' は素数ではない。')
